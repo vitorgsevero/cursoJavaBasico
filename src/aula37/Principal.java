8ee25af9415e282409634afdff13f471a680ab7a
Vitor Severo <vitorgsevero@gmail.com>
2018-03-19 22:05:55 -0300

    Herança

---
+++ src/aula37/Principal.java
@@ -0,0 +1,22 @@
+package aula37;
+
+public class Principal {
+    public static void main(String[] args) {
+     
+        Aluno aluno = new Aluno();
+        
+        Professor professor = new Professor();
+        
+        Pessoa pessoa = new Pessoa();
+        
+        aluno.setNome("Fulano"); 
+        
+        System.out.println(aluno);
+        
+        /*Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();*/
+        
+        
+        
+    }
+}
