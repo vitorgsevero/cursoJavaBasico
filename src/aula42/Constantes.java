f79e2279508110d6cedaf7f540ccf210a50469bb
Vitor Severo <vitorgsevero@gmail.com>
2018-03-19 22:19:18 -0300

    Palavra chave final

---
+++ src/aula42/Constantes.java
@@ -0,0 +1,10 @@
+package aula42;
+
+public final class Constantes {
+    
+    public static final String URL_GITHUB = "https://github.com/vitorgsevero";
+    public static final String URL_LINKEDIN = "https://www.linkedin.com/in/vitorgsevero/";
+    
+    
+    
+}
---
+++ src/aula42/Teste.java
@@ -0,0 +1,21 @@
+package aula42;
+
+public class Teste {
+   
+    public static void main(String[] args) {
+     
+   Pessoa pessoa = new Pessoa();
+  //Pessoa aluno = new Aluno();
+  //Pessoa professor = new Professor();
+  Aluno aluno = new Aluno();
+  Professor professor = new Professor();
+  //Constantes c = new Constantes();
+  
+  //segundo teste
+  
+   System.out.println(Constantes.URL_LINKEDIN); 
+   //Constantes.URL_GITHUB = "TESTANDO"; //Funciona apenas se a variável não for FINAL
+   
+  
+    }
+}
